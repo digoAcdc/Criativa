c33212163f00820183ea6b81d5084bda2250e336
digoacdc <barbosa_silveira@hotmail.com>
2016-08-26 07:33:35 -0300

    Login

---
+++ app/src/main/java/br/com/trasmontano/criativa/LoginActivity.java
@@ -0,0 +1,17 @@
+package br.com.trasmontano.criativa;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+
+}
